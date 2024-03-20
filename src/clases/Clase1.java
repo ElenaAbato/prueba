660e4a6ac879b5ac72026fe41702ad9a75158fd5
ElenaAbato <elenabato2004@gmail.com>
2024-03-20 17:19:40 +0100

    segundo commit

--- src/clases/Clase1.java
+++ src/clases/Clase1.java
@@ -5,4 +5,22 @@ package clases;
 public class Clase1 {
     private int numero;
     private String domicilio;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+    
+    
 }
--- src/clases/Clase2.java
+++ src/clases/Clase2.java
@@ -5,4 +5,22 @@ package clases;
 public class Clase2 {
     private int nombre;
     private int casa;
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCasa() {
+        return casa;
+    }
+
+    public void setCasa(int casa) {
+        this.casa = casa;
+    }
+    
+    
 }
